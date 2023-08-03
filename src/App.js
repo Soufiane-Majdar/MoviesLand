1c824b94716d24ffcb9b8f89b698e3ba58df3078
Soufiane-Majdar <soufianemj3145@gmail.com>
2023-08-04 00:42:36 +0200

    update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ import SearchIcon from "./search.svg";
 const API_URL = "http://www.omdbapi.com/?apikey=bb773d77";
 
 
+
+const movie1 = {
+    "Title": "Rogue One: A Star Wars Story",
+    "Year": "2016",
+    "imdbID": "tt3748528",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg"
+}
+
 const App = () => {
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -34,7 +43,25 @@ const App = () => {
                 <img
                     src={SearchIcon}
                     alt="search"
+                    onChange={() => { }}
                 />
+            </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+
+                    <div>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : "https://via.placeholder.com/400"} alt={movie1.Title} />
+                    </div>
+
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+                </div>
             </div>
         </div>
     )
